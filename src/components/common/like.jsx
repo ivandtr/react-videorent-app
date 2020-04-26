c7144612fa5cc7a16c410cd8784b22b828781ac2
idtr <ivanlozmar@gmail.com>
2020-04-26 13:26:06 +0200

    Turned Like into stateless funcitonal component

--- src/components/common/like.jsx
+++ src/components/common/like.jsx
@@ -1,16 +1,14 @@
 import React, { Component } from 'react';
 
-class Like extends Component {
-  render() {
-    let classes = this.props.liked ? 'fas fa-heart' : 'far fa-heart';
-    return (
-      <i
-        onClick={this.props.onClick}
-        style={{ cursor: 'pointer' }}
-        className={classes}
-      ></i>
-    );
-  }
-}
+const Like = (props) => {
+  let classes = props.liked ? 'fas fa-heart' : 'far fa-heart';
+  return (
+    <i
+      onClick={props.onClick}
+      style={{ cursor: 'pointer' }}
+      className={classes}
+    ></i>
+  );
+};
 
 export default Like;
