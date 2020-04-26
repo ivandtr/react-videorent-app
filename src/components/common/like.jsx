416afe7a68bccc17c1e301eaca7fdee865afb4c2
idtr <ivanlozmar@gmail.com>
2020-04-26 13:22:05 +0200

    Implemented handleLike method

--- src/components/common/like.jsx
+++ src/components/common/like.jsx
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
 
 class Like extends Component {
-  state = {};
   render() {
-    return <i class="fas fa-heart"></i>;
+    let classes = this.props.liked ? 'fas fa-heart' : 'far fa-heart';
+    return (
+      <i
+        onClick={this.props.onClick}
+        style={{ cursor: 'pointer' }}
+        className={classes}
+      ></i>
+    );
   }
 }
 
--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -7,6 +7,14 @@ class Movies extends Component {
     movies: getMovies(),
   };
 
+  handleLike = (movie) => {
+    const movies = [...this.state.movies];
+    const index = movies.indexOf(movie);
+    movies[index] = { ...movies[index] };
+    movies[index].liked = !movies[index].liked;
+    this.setState({ movies });
+  };
+
   handleDelete = (movie) => {
     const movies = this.state.movies.filter((m) => m._id !== movie._id);
     this.setState({ movies });
@@ -40,7 +48,10 @@ class Movies extends Component {
                 <td>{movie.numberInStock}</td>
                 <td>{movie.dailyRentalRate}</td>
                 <td>
-                  <Like />
+                  <Like
+                    liked={movie.liked}
+                    onClick={() => this.handleLike(movie)}
+                  />
                 </td>
                 <td>
                   <button
