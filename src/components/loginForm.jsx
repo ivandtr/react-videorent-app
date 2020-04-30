5eeda08619eeddfe0df085760197b6629bc2c446
idtr <ivanlozmar@gmail.com>
2020-04-30 20:02:28 +0200

    Added validation with Joi library

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,21 +1,25 @@
 import React, { Component } from 'react';
+import Joi from 'joi-browser';
 import Input from './common/input';
-
 class LoginForm extends Component {
   state = {
     account: { username: '', password: '' },
     errors: {},
   };
 
-  validate = () => {
-    const errors = {};
-    const { account } = this.state;
-    if (account.username.trim() === '')
-      errors.username = 'Username is required.';
-    if (account.password.trim() === '')
-      errors.password = 'Password is required.';
+  schema = {
+    username: Joi.string().required().label('Username'),
+    password: Joi.string().required().label('Password'),
+  };
 
-    return Object.keys(errors).length === 0 ? null : errors;
+  validate = () => {
+    const options = { abortEarly: false };
+    const { error } = Joi.validate(this.state.account, this.schema, options);
+    if (!error) return null;
+
+    const errors = {};
+    for (let item of error.details) errors[item.path[0]] = item.message;
+    return errors;
   };
 
   handleSubmit = (e) => {
@@ -23,20 +27,32 @@ class LoginForm extends Component {
 
     const errors = this.validate();
     console.log(errors);
-    this.setState({ errors });
+    this.setState({ errors: errors || {} });
     if (errors) return;
 
     console.log('Submitted');
   };
 
+  validateProperty = ({ id, value }) => {
+    const obj = { [id]: value };
+    const schema = { [id]: this.schema[id] };
+    const { error } = Joi.validate(obj, schema);
+    return error ? error.details[0].message : null;
+  };
+
   handleChange = ({ currentTarget: input }) => {
+    const errors = { ...this.state.errors };
+    const errorMessage = this.validateProperty(input);
+    if (errorMessage) errors[input.id] = errorMessage;
+    else delete errors[input.id];
+
     const account = { ...this.state.account };
     account[input.id] = input.value;
-    this.setState({ account });
+    this.setState({ account, errors });
   };
 
   render() {
-    const { account } = this.state;
+    const { account, errors } = this.state;
     return (
       <div>
         <h1>LoginForm</h1>
@@ -46,14 +62,18 @@ class LoginForm extends Component {
             value={account.username}
             label="Username"
             onChange={this.handleChange}
+            error={errors.username}
           />
           <Input
             name="password"
             value={account.password}
             label="Password"
             onChange={this.handleChange}
+            error={errors.password}
           />
-          <button className="btn btn-primary">Login</button>
+          <button disabled={this.validate()} className="btn btn-primary">
+            Login
+          </button>
         </form>
       </div>
     );
